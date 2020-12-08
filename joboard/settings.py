e03f43add4c0507ab7ab36fad4fb5d66c5c868ef
Varditli <vardit161@gmail.com>
2020-12-08 16:09:01 +0200

    Create models form for the notification feature

--- joboard/settings.py
+++ joboard/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'joboard_home.apps.JoboardHomeConfig',
-    'login'
+    'login',
+    'user_notification',
 ]
 
 MIDDLEWARE = [
---
+++ user_notification/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from . import models
+
+
+class CreateJobAlert(forms.ModelForm):
+    class Meta:
+        model = models.JobAlert
+        fields = ['UserAccount_id', 'alert_frequency', 'job_alert_type',
+                  'job_alert_scope', 'job_alert_city', 'job_alert_company_name']
