07b0f0b6384d316d8457e0e5101172e713ce08fe
mayasa3 <73421295+mayasa3@users.noreply.github.com>
2020-12-17 22:05:34 +0200

    Added URLs for log in and sign up forms

--- joboard/urls.py
+++ joboard/urls.py
@@ -1,25 +1,14 @@
-"""joboard URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
 from joboard_home import views
+from accounts import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^notification/', include('user_notification.urls')),
-    path('', views.home_page, name='home_page')
+    path('', views.home_page, name='home_page'),
+    path('accounts/', include('accounts.urls')),
+    path('signup/', user_views.signup_view, name='signup'),
+    path('login/', user_views.login_view, name='login'),
 ]
