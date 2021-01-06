cf1ae8dbe2db3654dcba1153604a717df0a6e93c
mayasa3 <73421295+mayasa3@users.noreply.github.com>
2021-01-06 15:58:33 +0200

    Updated HTML pages
    Added CSS and updated code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from joboard_home import views as home_views
 
 
 app_name = 'accounts'
@@ -8,4 +9,5 @@ app_name = 'accounts'
 urlpatterns = [
     url('signup/', views.sign_up_view, name='signup'),
     url('account/', views.account_view, name='account'),
+    url('home/', home_views.home_page, name='home'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,19 +5,21 @@ from django.contrib.auth.decorators import login_required
 
 
 def sign_up_view(request):
+    if request.user.is_authenticated:
+        return redirect('home_page')
     if request.method == 'POST':
         registration_form = SignUpForm(request.POST)
         if registration_form.is_valid():
             user = registration_form.save()
             user.save()
             login(request, user)
-            return redirect('accounts:account')
+            return redirect('home_page')
     else:
         registration_form = SignUpForm()
 
     return render(request, 'signup.html', {'registration_form': registration_form})
 
 
-@login_required
+@login_required(login_url='/login')
 def account_view(request):
     return render(request, 'account.html')
