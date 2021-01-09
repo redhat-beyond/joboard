2c0d206f1376fcb1bcaa1caf3de84f574a40ebca
mayasa3 <73421295+mayasa3@users.noreply.github.com>
2021-01-10 00:44:35 +0200

    Update profile
    Added to views and updated URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
-from joboard_home import views as home_views
 
 
 app_name = 'accounts'
@@ -10,6 +9,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     url('account/', views.account_view, name='account'),
-    url('home/', home_views.home_page, name='home'),
     path('login/', views.LoginView.as_view(), name='login'),
+    url('profile/', views.user_update, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from django.contrib import messages
 from django.contrib.auth import login
-from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView as BaseLogin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import UserAccount
+from django.contrib.auth.views import LoginView as BaseLogin
+from django.shortcuts import render, redirect
 from django.views.generic import View, FormView
+from .forms import SignUpForm, UserForm, AccountForm
 
 
 class LoginExcluded(View):
@@ -19,6 +21,7 @@ class SignUpView(FormView, LoginExcluded):
     form_class = SignUpForm
     template_name = 'signup.html'
 
+    # noinspection PyAttributeOutsideInit
     def post(self, request, *args, **kwargs):
         self.form = self.get_form()
         form = super(SignUpView, self).get_form(self.form_class)
@@ -39,3 +42,20 @@ class LoginView(BaseLogin, LoginExcluded):
 @login_required
 def account_view(request):
     return render(request, 'account.html')
+
+
+@login_required
+def user_update(request):
+    account = UserAccount.objects.get(user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        account_form = AccountForm(request.POST, instance=account)
+        if user_form.is_valid() and account_form.is_valid():
+            request.user = user_form.save()
+            account = account_form.save()
+            messages.success(request, 'User details updated.')
+
+    else:
+        user_form = UserForm()
+        account_form = AccountForm()
+    return render(request, 'profile.html', {'user_form': user_form, 'account_form': account_form})
